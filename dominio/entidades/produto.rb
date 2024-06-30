47e5d1f83776f9706ecfe2ca581d3b70ad5d7876
matheuspmbarcellos <matbarcellos21@gmail.com>
2024-06-30 00:24:19 -0300

    feat: create entity

---
+++ dominio/entidades/produto.rb
@@ -0,0 +1,12 @@
+class Produto # classe Anêmica
+    attr_accessor :id, :nome, :descricao, :preco, :quantidade
+    
+    def initialize(produto_hash)
+        self.id = produto_hash["id"]
+        self.nome = produto_hash["nome"]
+        self.descricao = produto_hash["descricao"]
+        self.preco = produto_hash["preco"]
+        self.quantidade = produto_hash["quantidade"]
+    end
+    
+end

30b2ae74ffaab51ee9a588959630028e83ab6c0c
matheuspmbarcellos <matbarcellos21@gmail.com>
2024-06-30 00:30:36 -0300

    feat: create execute

---
+++ init.rb
@@ -0,0 +1,11 @@
+require_relative "display/menu"
+require_relative "display/operacoes_em_tela"
+require_relative "display/servicos/cadastrar_produto"
+require_relative "display/servicos/listar_produto"
+require_relative "display/servicos/retirar_estoque"
+require_relative "dominio/entidades/produto"
+require_relative "dominio/servicos/produto_servico"
+require_relative "infraestrutura/json_repositorio"
+require_relative "infraestrutura/csv_repositorio"
+
+iniciar_menu
